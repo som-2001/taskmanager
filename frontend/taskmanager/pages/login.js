68bbfdbb7a4039b0bb7946a3da8ed7d06cc9c160
som-2001 <100964164+som-2001@users.noreply.github.com>
2024-05-06 23:02:49 +0530

    Update login.js

--- frontend/taskmanager/pages/login.js
+++ frontend/taskmanager/pages/login.js
@@ -26,7 +26,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('taskmanager-khaki-five.vercel.app/api/login', formData);
+            const response = await axios.post('https://taskmanager-khaki-five.vercel.app/api/login', formData);
             console.log(response);
             if(response.data.message ==='Login successful')
                 {
