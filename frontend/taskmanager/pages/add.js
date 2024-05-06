1c6c6891264d9dd8ccf00cfa65636f91990b74df
som-2001 <100964164+som-2001@users.noreply.github.com>
2024-05-06 21:28:26 +0530

    Update add.js

--- frontend/taskmanager/pages/add.js
+++ frontend/taskmanager/pages/add.js
@@ -27,7 +27,7 @@ const Add = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('taskmanager-lyart.vercel.app/api/register', formData);
+            const response = await axios.post('taskmanager-khaki-five.vercel.app/api/register', formData);
             setTimeout(()=>{
                 if(response.data==='successfully registered')
                 router.push('/login')
